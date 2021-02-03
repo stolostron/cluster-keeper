0d9d7ef3bae50304d2ed87972cb897851cb26d4a
Kevin Cormier <kcormier@redhat.com>
2021-02-03 09:29:54 -0500

    Support infer claim for delete

--- subcommands/delete.sh
+++ subcommands/delete.sh
@@ -4,11 +4,11 @@ function delete_description {
 }
 
 function delete_usage {
-  errEcho "usage: $(basename ${0}) delete [OPTIONS] CLAIM"
+  errEcho "usage: $(basename ${0}) delete [OPTIONS] [CONTEXT]"
   errEcho
   errEcho "    $(delete_description)"
   errEcho
-  errEcho "    CLAIM is the name for the new ClusterClaim"
+  errEcho "    CONTEXT is the name of a kube context that matches a ClusterClaim"
   errEcho
   errEcho "    The following OPTIONS are available:"
   errEcho
@@ -30,20 +30,26 @@ function delete {
   done
   shift $(($OPTIND - 1))
 
-  if [[ -z $1 ]]
+  local context=$1
+  if [[ -z $context ]]
   then
-    delete_usage
+    context=$(current)
   fi
 
+  # if [[ -z $1 ]]
+  # then
+  #   delete_usage
+  # fi
 
-  local clusterDeployment=$(getClusterDeployment $1)
+
+  local clusterDeployment=$(getClusterDeployment $context)
   if [[ -n $clusterDeployment ]]
   then
-    checkLocks $1
+    checkLocks $context
   fi
 
   # Use lifeguard/clusterclaims/delete.sh to delete ClusterClaim
-  export CLUSTERCLAIM_NAME=$1
+  export CLUSTERCLAIM_NAME=$context
   export CLUSTERPOOL_TARGET_NAMESPACE
   if [[ -z "$CONFIRM_DELETION" ]]
   then
