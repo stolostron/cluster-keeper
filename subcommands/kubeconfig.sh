ebc8eddd0f8308ce09dcaa68815c1a94d7fbe615
Kevin Cormier <kcormier@redhat.com>
2021-02-17 10:16:25 -0500

    Implement kubeconfig subcommand

---
+++ subcommands/kubeconfig.sh
@@ -0,0 +1,43 @@
+# Command for displaying location of kubeconfig file
+function kubeconfig_description {
+  echo "Display the location of the kubeconfig file"
+}
+
+function kubeconfig_usage {
+  errEcho "usage: $(basename ${0}) pw [OPTIONS] [CONTEXT]"
+  errEcho
+  errEcho "    $(kubeconfig_description)"
+  errEcho
+  errEcho "    CONTEXT is the name of a kubeconfig context that matches a ClusterClaim"
+  errEcho
+  errEcho "    The following OPTIONS are available:"
+  errEcho
+  errEcho "    -r    Refresh the kubeconfig by fetching a fresh copy"
+  errEcho
+  abort
+}
+
+function kubeconfig {
+  OPTIND=1
+  while getopts :r o 
+  do case "$o" in
+    r)  FETCH_FRESH="true";;
+    [?]) creds_usage;;
+    esac
+  done
+  shift $(($OPTIND - 1))
+
+  local context=$1
+  if [[ -z $context ]]
+  then
+    context=$(current)
+  fi
+  case $context in
+    cm)
+      fatal "Cannot show kubeconfig file location for the ClusterPool host"
+      ;;
+    *)
+      showKubeconfig $context $FETCH_FRESH
+      ;;
+  esac
+}
