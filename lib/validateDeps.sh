9b0bc822ca674ab9f49147f58f793b11fbdedeb8
Kevin Cormier <kcormier@redhat.com>
2021-05-19 10:03:19 -0400

    - Source logging.sh only once and move required variables there - Move configuration file handling to validateDeps.sh - Make messages consistent - Remove shebang from library files
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- lib/validateDeps.sh
+++ lib/validateDeps.sh
@@ -1,13 +1,14 @@
-#!/usr/bin/env bash
 # Copyright Contributors to the Open Cluster Management project
 
-source ${DIR}/lib/logging.sh
+# Validate configuration file is present and load
+[[ -f $DIR/user.env ]] || fatal "user.env file not found"
+. $DIR/user.env
 
 # Validate bash version is 4 or greater
 if [ "${BASH_VERSINFO:-0}" -lt 4 ]
 then
-  error "DEPENDENCY NOT MET. bash version 4 or greater is required to run this tool. Found version ${BASH_VERSION}"
-  verbose 0 "On MacOS install with \"brew install bash\".  This bash must be first in your path, but need not be '/bin/bash' or your default login shell."
+  error "bash version 4 or greater is required to run this tool. Found version ${BASH_VERSION}"
+  verbose 0 "On macOS install with \"brew install bash\".  This bash must be first in your path, but need not be '/bin/bash' or your default login shell."
   abort
 fi
 
@@ -17,7 +18,7 @@ OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 if [ "${OS}" == "darwin" ]; then
     if [ ! -x "$(command -v jq)"  ]; then
        error "jq is required, but not found."
-       verbose 0 "On MacOS install with \"brew install jq\" and try again."
+       verbose 0 "On macOS install with \"brew install jq\" and try again."
        abort
     fi
 fi
@@ -27,7 +28,7 @@ fi
 if [ "${OS}" == "darwin" ]; then
     if [ ! -x "$(command -v gsed)"  ]; then
        error "gsed is required, but not found."
-       verbose 0 "On MacOS install with \"brew install gnu-sed\" and try again."
+       verbose 0 "On macOS install with \"brew install gnu-sed\" and try again."
        abort
     fi
 fi
